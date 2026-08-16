2830308a5644874ccf31903c5060e42f17652742
llhomosapient-debug <llhomosapient@gmail.com>
2026-08-16 23:24:04 +0200

    history.js

--- js/history.js
+++ js/history.js
@@ -1,1 +1,10 @@
+function getChats() {
+  return JSON.parse(
+    localStorage.getItem("nova_chats") || "[]"
+  )
+}
 
+function clearChats() {
+  localStorage.removeItem("nova_chats")
+  location.reload()
+}
